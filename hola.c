1224a49116865b00179bae5f90b8e9c8b1650477
victorbrull <victor.brull01@estudiant.upf.edu>
2024-04-27 23:13:49 +0200

    a

--- hola.c
+++ hola.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-int main{
-    fprintf("Holaa");
+typedef struct Skills {
+    char skill_1[15];
+    char skill_2[15];
+    char skill_3[15];
+    char skill_4[15];
+} Skills;
+
+typedef struct Character {
+    char name[15];
+    int hp;
+    int attack;
+    int defence;
+    struct Skills skills;
+} Character;
+
+typedef struct Enemy {
+    char name[15];
+    int hp;
+    int attack;
+    int defence;
+    struct Skills skills;
+} Enemy;
+
+int main() {
     return 0;
 }
