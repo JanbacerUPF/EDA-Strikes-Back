7fa6dc1981b1661c3645fcaa4ec0d4f4015b2c28
victorbrull <victor.brull01@estudiant.upf.edu>
2024-05-08 11:31:08 +0200

    changes

--- main.c
+++ main.c
@@ -1,11 +1,38 @@
 #include "combat.c"
-#include "struct.h"
+#include "struct.c"
 
-int run_game() {
-    return 0;
+#define NEW_GAME 1
+#define EXIT 2
+
+
+void new_game(){
+    Character player = character_creation();
 }
 
+int menu(){
+    printf("___  ___                 \n");
+    printf("|  \\/  |                 \n");
+    printf("| .  . | ___ _ __  _   _ \n");
+    printf("| |\\/| |/ _ \\ '_ \\| | | |\n");
+    printf("| |  | |  __/ | | | |_| |\n");
+    printf("\\_|  |_|\\___|_| |_|\\__,_|\n");
+    printf("                         \n");
+    printf("                         \n");
+    int option;
+    while(option!=EXIT){
+        printf("SELECT AN OPTION\n");
+        printf("1. New Game\n");
+        printf("2. Exit Game\n");
+        scanf("%d",&option);
+        if(option==NEW_GAME){
+            new_game();
+        }
+    }
+
+}
+
+
 int main(){
-    run_game();
+    menu();
     return 0;
 }
