cbf83fa25069e384ad3eda1006fc74959760b3e7
victorbrull <victor.brull01@estudiant.upf.edu>
2024-05-08 11:15:12 +0200

    combat

--- combat.c
+++ combat.c
@@ -1,5 +1,8 @@
 #include "struct.h"
 
-bool is_finished(Character character, Enemy enemies) {
+bool is_finished(Character* player, Enemy* enemies) {
+    if (player->hp != 0 && enemies->hp == 0) {
+        return true;
+    }
     return false;
 }
